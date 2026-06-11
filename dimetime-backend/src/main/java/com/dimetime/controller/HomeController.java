688002098bf7fbd7b75c1b710dab6f4c2b266a22
Parth-2374 <Parthchaudhary2004@gmail.com>
2026-06-11 14:37:43 +0530

    Dime-Time-Backend

---
+++ dimetime-backend/src/main/java/com/dimetime/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.dimetime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "DimeTime Backend Running Successfully";
+    }
+}
